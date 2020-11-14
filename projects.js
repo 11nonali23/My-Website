e4bc1450427b4dfb93e7bd7d6a58e7824cc48440
Andrea Nonali <11nonali23@gmal.com>
2020-11-14 17:56:57 +0100

    prettified project page with flexbox

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@
 var isRight = true;
 
 //Movement Animation to happen
-const card1 = document.querySelector(".card");
+const card1 = document.querySelector(".card1");
 const card2 = document.querySelector(".card2");
 const card3 = document.querySelector(".card3");
 
