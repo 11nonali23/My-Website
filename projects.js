ca3f9c4a467cddfe7c3fe287c1c49c250bfba1e1
Andrea Nonali <11nonali23@gmal.com>
2020-11-17 10:56:32 +0100

    removed animetions when searching on phone

--- projects.js
+++ projects.js
@@ -1,5 +1,8 @@
+//GLOBAL VARIABLES
 //to rotate images to both sides
 var isRight = true;
+//Same width of css media query that triggers when elements are wrapped
+var max_width = 1391;
 
 //Movement Animation to happen
 const card1 = document.querySelector(".card1");
@@ -9,6 +12,10 @@ const card3 = document.querySelector(".card3");
 function addCardListener(card){
     //Moving Animation Event
     card.addEventListener("mousemove", (e) => {
+        // does not trigger animation if elements are wrapped
+        if ( $(window).width() < max_width) {  
+            return;
+        }
         let xAxis = (window.innerWidth / 2 - e.pageX) / 45;
         let yAxis = (window.innerHeight / 2 - e.pageY) / 45;
         card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
